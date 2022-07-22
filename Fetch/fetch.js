bad40b82eb80e311a1376453199700c172265bf0
franncottet <cottetfranco@gmail.com>
2022-07-22 18:33:47 -0300

    desafío Fetch Terminado

---
+++ Fetch/fetch.js
@@ -0,0 +1,12 @@
+let contenido= document.getElementById("content")
+        
+        function generar(){
+        fetch("https://randomuser.me/api/")
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data.results["0"]);
+            contenido.innerHTML = `
+            <img src="${data.results["0"].picture.large}" width="100px" class="img-fluid rounded-circle">
+            <p>Nombre: ${data.results["0"].name.first}</p>`
+        })
+       }
