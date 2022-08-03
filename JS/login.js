89b46061d8e815d262a206a46d704d89f1c16cd9
franncottet <cottetfranco@gmail.com>
2022-08-03 15:41:43 -0300

    Actualizaciones

--- JS/login.js
+++ JS/login.js
@@ -1,3 +1,87 @@
+const formulario = document.getElementById("formulario")
+
+const userName=document.getElementById("userName")
+const userEmail=document.getElementById("userEmail")
+
+const alertSuccess=document.getElementById("alertSuccess")
+const EmailError=document.getElementById("EmailError")
+const NameError=document.getElementById("NameError")
+
+const regUserName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+const regUserEmail = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/
+
+const mensajeExito= () =>{
+    alertSuccess.classList.remove("d-none")
+    alertSuccess.textContent="Tu cuenta ha sido creada con éxito. Ya puedes solicitar prestamos"
+}
+
+const mensajeError = (errores)=>{
+errores.forEach(i=>{
+    i.tipo.classList.remove("d-none")
+    i.tipo.textContent=i.msj
+    
+})
+}
+
+formulario.addEventListener("submit", e=>{
+
+    alertSuccess.classList.add("d-none")
+
+    //e.preventDefault();
+
+    const errores=[];
+
+
+
+if (!regUserName.test(userName.value)) {
+    userName.classList.add("is-invalid")
+    errores.push({
+        tipo: NameError,
+        msj: "Formato no válido en el campo nombre, solo letras"
+    })
+}else{
+    userName.classList.remove("is-invalid")
+    userName.classList.add("is-valid")
+    NameError.classList.add("d-none")
+}
+
+if (!regUserEmail.test(userEmail.value)) {
+    userEmail.classList.add("is-invalid")
+    errores.push({
+        tipo: EmailError,
+        msj: "Ingrese un correo válido"
+    })
+}else{
+    userEmail.classList.remove("is-invalid")
+    userEmail.classList.add("is-valid")
+    EmailError.classList.add("d-none")
+}
+
+if (errores.lenght!==0) {
+    mensajeError(errores)
+    return
+}
+
+mensajeExito()
+console.log("formulario enviado");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 let boton = document.getElementById("trabajo")
 
 function getSelectValue(){
@@ -18,3 +102,4 @@ function getSelectValue(){
 
 boton.addEventListener("click", getSelectValue)
 
+*/
--- JS/simulador.js
+++ JS/simulador.js
@@ -11,3 +11,45 @@ function calculoInteres() {
 
 boton.addEventListener("click", calculoInteres)
 
+
+
+
+
+
+
+/*
+
+//INTERES ==> 72% de interes anual (6% mensual=0,06)
+
+
+
+let bienvenida=prompt("Bienvenido a la aplicacion para calcular prestamos. Escriba OK para continuar, de otra manera, el sistema se cerrará");
+
+
+if(bienvenida ==="OK" || bienvenida ==="Ok" || bienvenida ==="ok" ){
+    // Ingreso dato de dinero
+    let dinero=parseInt(prompt("¿Cuanta cantidad de dinero quiere recibir? Recuerde que el mínimo es $5.000 y el máximo es $100.000"));
+    //Proceso el dato del dinero
+    if (dinero <=100000 && dinero>=5000) {
+        //Si el monto ingresado cumple la condicion.. calculo los intereses
+        //Creo la función
+        function Calculointeres() {
+            //Ingreso dato del dinero
+            let interes=dinero*0.06
+            //Creo una función que procese los datos ingresados por el usuario
+            function Calculocuotafinal() {
+                let cuotas=parseInt(prompt("¿En cuantas cuotas lo vas a devolver? Desde 3 cuotas a 24 cuotas"));
+                cuotamensual=(dinero/cuotas)+interes;
+                return cuotamensual;
+            };
+            //Llamo a la función creada.
+            return Calculocuotafinal();
+        }
+        //Llamo a la función que cree + salida de los datos.
+        alert("Deberas abonar " + Calculointeres() + " por mes");
+    }
+    
+} else {
+    alert ("Gracias, vuelva pronto");
+}
+*/
