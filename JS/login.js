6da6b68d41a8d470303bb2f69c73d03baeda154d
franncottet <cottetfranco@gmail.com>
2022-08-10 16:06:37 -0300

    actualizacion final

--- JS/login.js
+++ JS/login.js
@@ -1,14 +1,24 @@
 const formulario = document.getElementById("formulario")
 
-const userName=document.getElementById("userName")
-const userEmail=document.getElementById("userEmail")
+const userName=document.getElementById("userName");
+const userEmail=document.getElementById("userEmail");
+const edad= document.getElementById("edad");
+const trabajo = document.getElementById("trabajo");
+const antiguedad= document.getElementById("antiguedad");
+const deuda = document.getElementById("deuda");
 
 const alertSuccess=document.getElementById("alertSuccess")
 const EmailError=document.getElementById("EmailError")
 const NameError=document.getElementById("NameError")
+const edadError= document.getElementById("edadError")
 
 const regUserName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-const regUserEmail = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/
+const regUserEmail = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
+const regEdad = /^\d+$/gi;
+
+
+
+
 
 const mensajeExito= () =>{
     alertSuccess.classList.remove("d-none")
@@ -21,6 +31,10 @@ errores.forEach(i=>{
     i.tipo.textContent=i.msj
     
 })
+}
+
+function mostrarAntiguedad() {
+    antiguedad.classList.remove("d-none")
 }
 
 formulario.addEventListener("submit", e=>{
@@ -45,6 +59,18 @@ if (!regUserName.test(userName.value)) {
     NameError.classList.add("d-none")
 }
 
+if (!regEdad.test(edad.value) || edad<18) {
+    edad.classList.add("is-invalid")
+    errores.push({
+        tipo: edadError,
+        msj: "Ingresa una edad válida. Recuerda que debes ser mayor de 18 años."
+    })
+}else{
+    edad.classList.remove("is-invalid")
+    edad.classList.add("is-valid")
+    edadError.classList.add ("d-none")
+}
+
 if (!regUserEmail.test(userEmail.value)) {
     userEmail.classList.add("is-invalid")
     errores.push({
@@ -62,6 +88,12 @@ if (errores.lenght!==0) {
     return
 }
 
+if (trabajo.value==="1") {
+    mostrarAntiguedad()
+    return
+}
+
+mostrarAntiguedad()
 mensajeExito()
 console.log("formulario enviado");
 })
